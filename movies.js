ab717e4bea4204d35b012fb0e362f817cb4b9293
Crystal Tran <c.t.tran95@gmail.com>
2023-11-13 21:01:27 -0500

    completed movie app

--- movies.js
+++ movies.js
@@ -5,7 +5,16 @@ $('#movie-input').on('submit', addMovieToRatingsList);
 function addMovieToRatingsList(evt){
   evt.preventDefault();
   console.log('adding movie to list');
-  const titleInput = $('#title-input').val();
-  const ratingInput = $('#rating-input').val();
-  console.log('logging movie, title:', titleInput, 'rating:', ratingInput);
+  const $titleInput = $('#title-input').val();
+  const $ratingInput = $('#rating-input').val();
+  const $deleteBtn = $("<button>").text("Delete");
+  $deleteBtn.addClass('btn btn-dark click')
+
+  $('#movie-ratings').append($("<div>").text(`${$titleInput} ${$ratingInput}`).append($deleteBtn));
+  //console.log('logging movie, title:', titleInput, 'rating:', ratingInput);
 }
+
+$('#movie-ratings').on('click', 'button', function(evt){
+  evt.preventDefault();
+  $(evt.target).parent().remove();
+})
